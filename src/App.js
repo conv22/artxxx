dfde3ca150e3d3688bd1d418745b276412038712
conv22 <metoneup@gmail.com>
2021-03-15 13:08:42 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,113 +1,110 @@
-import React, {useState, useEffect, useRef, useCallback} from "react"
-import './App.css';
-import { Header } from "./Components/Header";
-import { MovieList } from "./Components/MovieList";
-import { Navbar } from "./Components/Navbar";
-import { Paginator } from "./Components/Paginataor";
+import React, { useState, useEffect, useRef, useCallback } from "react"
+import "./App.css"
+import { Header } from "./Components/Header"
+import { MovieList } from "./Components/MovieList"
+import { Navbar } from "./Components/Navbar"
+import { Paginator } from "./Components/Paginataor"
 import InfiniteScroll from "react-infinite-scroll-component"
 
-
-
 function App() {
-  let myRef = useRef()
+    let myRef = useRef()
 
-  const [numberPage, setnumberPage] = useState(1)
-  const [paginator, setPaginator] = useState(false)
+    const [numberPage, setnumberPage] = useState(1)
+    const [sortOption, setsortOption] = useState("popularity.desc")
 
-  const [loading, setloading] = useState(false)
-  const [movies, setMovies] = useState({
-  page: numberPage,
-  results: [],
-  total_pages: 0,
-  total_results: 0,
-})
-
-const [sortOption, setsortOption] = useState('popularity.desc')
-
-// useEffect(()=>{
-//   document.addEventListener('scroll', handleScroll)
-//   return () => document.removeEventListener('scroll', handleScroll)
-// }, [])
-// const handleScroll = (e) => {
-//   if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight)<50) {
-//   setTimeout(() => setloading(true), 500)
-//     }
-//   }
-
-
-  let loadMovies = (p=1) => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?page=${p}&sort_by=${sortOption}&api_key=4237669ebd35e8010beee2f55fd45546`)
-    .then(res=>res.json())
-    .then(data=>{
-      setMovies(data)
-       setloading(false)
-          setloading(true)
-      setTimeout(() => setnumberPage(data.page), 700)
-      setTimeout(() => setPaginator(false), 700)
+    const [loading, setloading] = useState(false)
+    const [movies, setMovies] = useState({
+        page: numberPage,
+        results: [],
+        total_pages: 0,
+        total_results: 0,
     })
-    .catch(err=>console.log("errorFetch", err))
-  }
 
-let loadFilms = () => {
-  fetch(`https://api.themoviedb.org/3/discover/movie?page=${numberPage+1}&sort_by=${sortOption}&api_key=4237669ebd35e8010beee2f55fd45546`)
-  .then(res=>res.json())
-  .then(data=>{
-    setMovies({
-      page: data.page,
-      results: [...movies.results, ...data.results],
-      total_pages: data.total_pages,
-      total_results: data.total_results,
-  })
-    //setloading(false)
-    setnumberPage(data.page)
-  })
-  .catch(err=>console.log("errorFetch", err))
-}
+    const loadMovies = useCallback(
+        (p = 1) => {
+            console.log("default pagination")
+            setloading(true)
+            fetch(
+                `https://api.themoviedb.org/3/discover/movie?page=${p}&sort_by=${sortOption}&api_key=4237669ebd35e8010beee2f55fd45546`
+            )
+                .then(res => res.json())
+                .then(data => {
+                    setloading(false)
+                    setMovies(data)
+                    setnumberPage(data.page)
+                })
+                .catch(err => {
+                    setloading(false)
+                    console.log("errorFetch", err)
+                })
+        },
+        [sortOption]
+    )
 
+    const loadFilms = useCallback(() => {
+        console.log("on scroll")
+        setloading(true)
+        fetch(
+            `https://api.themoviedb.org/3/discover/movie?page=${
+                numberPage + 1
+            }&sort_by=${sortOption}&api_key=4237669ebd35e8010beee2f55fd45546`
+        )
+            .then(res => res.json())
+            .then(data => {
+                setloading(false)
+                setMovies({
+                    page: data.page,
+                    results: [...movies.results, ...data.results],
+                    total_pages: data.total_pages,
+                    total_results: data.total_results,
+                })
 
+                setnumberPage(numberPage + 1)
+            })
+            .catch(err => {
+                setloading(false)
+                console.log("errorFetch", err)
+            })
+    }, [movies.results, sortOption, numberPage])
 
-useEffect(() => {
- if (loading) {
-  loadFilms()
-}
-}, [loading])
+    useEffect(() => {
+        loadMovies()
+    }, [loadMovies])
+    useEffect(() => {
+        console.log(movies)
+    }, [movies])
 
-useEffect(()=>{
-  loadMovies()
-}, [sortOption])
-
-let handlerScroll = () => {
- if (!paginator) {
-    loadFilms()
-  }
-  //setnumberPage(numberPage+1)
-}
-console.log(paginator)
-
-  return (
-    <div >
-    <Header/>
-    <Navbar setsortOption={setsortOption}  loadMovies={loadMovies}/>
-    <div className={'body'}>
-    <div className={'wrap_paginnation'}>
-      <Paginator setPaginator={setPaginator} sortOption={sortOption} setnumberPage={setnumberPage} page={numberPage} loadMovies={loadMovies} total_pages={movies.total_pages}/>
-    </div>
-    </div>
-    <InfiniteScroll
-                    dataLength={movies.results.length}
-                    next={handlerScroll}
-                    hasMore={true}
-                    loader={<h1>Loading...</h1>}>
-    <div name="test3" className={'listMovie'} >
-        { movies.results && movies.results.map((movie,i)=> 
-        <div ref={myRef} key={i}>
-              <MovieList movie={movie}/>
+    return (
+        <div>
+            <Header />
+            <Navbar setsortOption={setsortOption} loadMovies={loadMovies} />
+            <div className={"body"}>
+                <div className={"wrap_paginnation"}>
+                    <Paginator
+                        sortOption={sortOption}
+                        setnumberPage={setnumberPage}
+                        page={numberPage}
+                        loadMovies={loadMovies}
+                        total_pages={movies.total_pages}
+                    />
+                </div>
+            </div>
+            <InfiniteScroll
+                dataLength={movies.results.length}
+                next={loadFilms}
+                hasMore={true}
+                loader={<h1>Loading...</h1>}>
+                <div name="test3" className={"listMovie"}>
+                    {movies.results &&
+                        movies.results.map((movie, i) => (
+                            <div ref={myRef} key={i}>
+                                <MovieList movie={movie} />
+                            </div>
+                        ))}
+                </div>
+            </InfiniteScroll>
         </div>
-          )}
-    </div>
-    </InfiniteScroll>
-    </div>
-  );
+    )
 }
 
-export default App;
+export default App
--- src/Components/Paginataor.jsx
+++ src/Components/Paginataor.jsx
@@ -1,83 +1,68 @@
-import style from'./paginator.module.css'
-import { useState } from "react";
+import style from "./paginator.module.css"
+import { useState, useEffect } from "react"
 
+export function Paginator({ sortOption, page, setnumberPage, total_pages, loadMovies, setPaginator }) {
+    const onPageChanged = p => {
+        window.scrollTo(0, 0)
+        loadMovies(p)
+        setnumberPage(p)
+    }
+    const [pages, setPages] = useState([])
 
+    const portionSize = 5
+    const portionCount = Math.ceil(total_pages / portionSize)
+    const [portionNumber, setPortionNumber] = useState(1)
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    const rightPortionPageNumber = portionNumber * portionSize
 
+    useEffect(() => {
+        console.log(1)
+        const loadPages = () => {
+            const pages = []
+            for (let i = 1; i <= total_pages; i++) {
+                pages.push(i)
+            }
+            setPages(pages)
+        }
+        loadPages()
+    }, [total_pages])
 
-
-export function Paginator({
-    sortOption,
-  page,
-  setnumberPage,
-  total_pages,
-  loadMovies,
-  setPaginator
-}) {
-
-  let onPageChanged = (p) => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-    loadMovies(p)
-    setnumberPage(p)
-      setPaginator(true)
-  };
-
-  let portionSize = 5;
-  let portionCount = Math.ceil(total_pages / portionSize);
-  let [portionNumber, setPortionNumber] = useState(1);
-  let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-  let rightPortionPageNumber = portionNumber * portionSize;
-
-  let pages = [];
-  for (let i = 1; i <= total_pages; i++) {
-    pages.push(i);
-  }
-
-
-  return (
-    <div className={style.pagination}>
-      <div>
-        {portionNumber > 1 && (
-          <div
-            onClick={() => {
-              setPortionNumber(portionNumber - 1);
-              onPageChanged(leftPortionPageNumber-1)
-            }}
-          >
-            пред
-          </div>
-        )}
-        <div>
-          {pages
-            .filter(
-              (p) => p >= leftPortionPageNumber && p <= rightPortionPageNumber
-            )
-            .map((p) => {
-              return (
-                <span
-                  className={
-                    page === p ? style.selectNumberPage : style.numberPage
-                  }
-                  key={p}
-                  onClick={(e) => {
-                    onPageChanged(p);
-                  }}
-                >
-                  {p}
-                </span>
-              );
-            })}
+    return (
+        <div className={style.pagination}>
+            <div>
+                {portionNumber > 1 && (
+                    <div
+                        onClick={() => {
+                            setPortionNumber(portionNumber - 1)
+                            onPageChanged(leftPortionPageNumber - 1)
+                        }}>
+                        пред
+                    </div>
+                )}
+                <div>
+                    {pages
+                        .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                        .map(p => {
+                            return (
+                                <span
+                                    className={page === p ? style.selectNumberPage : style.numberPage}
+                                    key={p}
+                                    onClick={() => onPageChanged(p)}>
+                                    {p}
+                                </span>
+                            )
+                        })}
+                </div>
+                {portionCount > portionNumber && (
+                    <div
+                        onClick={() => {
+                            setPortionNumber(portionNumber + 1)
+                            onPageChanged(rightPortionPageNumber + 1)
+                        }}>
+                        след
+                    </div>
+                )}
+            </div>
         </div>
-        {portionCount > portionNumber && (
-          <div
-            onClick={() => {
-              setPortionNumber(portionNumber + 1);
-              onPageChanged(rightPortionPageNumber+1)
-            }}
-          >
-            след
-          </div>
-        )}
-      </div>
-    </div>
-  );
+    )
 }
